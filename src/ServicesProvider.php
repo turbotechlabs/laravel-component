bb8af2e1870981d718d756983d4d2d96598438dd
username <sophat.leat@turbotech.com>
2023-05-19 09:58:43 +0700

    Update Button Folder

--- src/ServicesProvider.php
+++ src/ServicesProvider.php
@@ -25,19 +25,19 @@ class ServicesProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/views/panels', 'panels');
-        $this->loadViewsFrom(__DIR__.'/views/element', 'elements');
+        $this->loadViewsFrom(__DIR__.'/views/element/buttons', 'buttons');
 
         /* Main Card */ 
         Blade::component('panels::main-card', 'main-card');
         Blade::component('panels::title', 'card:title');
 
         /* Element Button */ 
-        Blade::component('elements::button', 'button');
-        Blade::component('elements::button-primary', 'button:primary');
-        Blade::component('elements::button-secondary', 'button:secondary');
-        Blade::component('elements::button-warning', 'button:warning');
-        Blade::component('elements::button-danger', 'button:danger');
-        Blade::component('elements::button-success', 'button:success');
+        Blade::component('buttons::button', 'button');
+        Blade::component('buttons::button-primary', 'button:primary');
+        Blade::component('buttons::button-secondary', 'button:secondary');
+        Blade::component('buttons::button-warning', 'button:warning');
+        Blade::component('buttons::button-danger', 'button:danger');
+        Blade::component('buttons::button-success', 'button:success');
         
     }
 
